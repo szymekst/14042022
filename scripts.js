514958026da2201e956cdfc6249afbe9db36d753
Szymon <79141168+C4US3@users.noreply.github.com>
2022-04-22 02:39:14 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,42 @@
+function isChecked() {
+	"use strict";
+	let status = document.querySelector('#chbxStatus');
+	let spans = document.getElementsByClassName("mnth");
+	
+	if(status.checked === true) {
+		document.getElementById("yearly").style.color = "#FFF";
+		document.getElementById("monthly").style.color = "#ADADAD";
+		document.getElementById("f-price").innerHTML = "$86.4"
+		document.getElementById("s-price").innerHTML = "$216"
+		document.getElementById("t-price").innerHTML = "$518.4"
+		for(var i=0; i < spans.length; i++) {
+			spans[i].innerHTML = "Year";
+		}
+	}
+	else {
+		document.getElementById("yearly").style.color = "#ADADAD";
+		document.getElementById("monthly").style.color = "#FFF";
+		document.getElementById("f-price").innerHTML = "$8"
+		document.getElementById("s-price").innerHTML = "$20"
+		document.getElementById("t-price").innerHTML = "$48"
+		for(var i=0; i < spans.length; i++) {
+			spans[i].innerHTML = "Month";
+		}
+	}
+}
+
+function showMenu() {
+	document.getElementById("menu").style.right = 0;
+	document.getElementById("header").style.background = "#000";
+	
+	document.getElementById("bars").style.cssText = "display: none !important";
+	document.getElementById("close").style.cssText = "display: block !important";
+}
+
+function closeMenu() {
+	document.getElementById("menu").style.right = "-100vw";
+	document.getElementById("header").style.background = "transparent";
+
+	document.getElementById("bars").style.cssText = "display: block !important";
+	document.getElementById("close").style.cssText = "display: none !important";
+}
